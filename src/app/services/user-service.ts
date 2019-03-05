8f006c2b5088429a81e8ad9038a75837d9173dc9
dussau40 <44094835+dussau40@users.noreply.github.com>
2019-03-05 16:38:03 +0100

    Add files via upload

---
+++ src/app/services/user-service.ts
@@ -0,0 +1,20 @@
+import { User } from '../models/User.model';
+import { Subject } from 'rxjs/Subject';
+
+export class UserService {
+  private users: User[] = [
+    new User('Dussau', 'Kévin', 'kevin.dussau@soprasteria.com', 'Ice Tea', ['coder', 'boire du café'])
+];
+  userSubject = new Subject<User[]>();
+
+  emitUsers() {
+    this.userSubject.next(this.users.slice());
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
+    this.emitUsers();
+  }
+
+  
+}
